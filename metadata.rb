96f7adfab98d3c4c8ac8f4c5df746b08c4cacb55
Cody Dracars <cdracars@gmail.com>
2013-01-14 13:54:37 -0600

    Foodcritic FC007 & FC045

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
+name             "site-backup"
 maintainer       "Cody Dracars"
 maintainer_email "cdracars@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures Drupal Solr Setup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+depends          "s3cmd"
